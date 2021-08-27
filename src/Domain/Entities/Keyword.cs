18ec20cedfb1437685a5df54e8816aeed8baf2c7
burkovski <burkovski.danil@icloud.com>
2021-08-27 22:09:48 +0300

    MT-428 fix: Reorganize Entities package
    HashedKeywordsContext class was removed due to new domain-specific demands.
    
    See info in comments to task

--- src/Domain/Entities/Keyword.cs
+++ src/Domain/Entities/Keyword.cs
@@ -1,12 +1,16 @@
+using System.Text;
+
 namespace Domain.Entities
 {
-    public class Keyword
+    public record Keyword(int Id, string CountryCode, string SearchString, long? Hash)
     {
-        public string CountryCode { get; set; }
-
-        public string SearchString { get; set; }
-
-        public override string ToString() =>
-            $"CountryCode={CountryCode} SearchString={SearchString}";
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("Id={0} CountryCode={1} SearchString={2}", Id, CountryCode, SearchString);
+            if (Hash.HasValue)
+                stringBuilder.AppendFormat(" Hash={0}", Hash.Value);
+            return stringBuilder.ToString();
+        }
     }
 }
