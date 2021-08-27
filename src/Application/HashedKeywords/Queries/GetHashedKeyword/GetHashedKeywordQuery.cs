7eb9f9ce1c4f34cfe1e54ad64c37d011da2a730f
burkovski <burkovski.danil@icloud.com>
2021-08-27 22:19:22 +0300

    MT-428 fix: Remove unused query GetHashedKeywordQuery and handler for this query
    These classes are redundant in current solution
    
    See more info in comments to task

--- src/Application/HashedKeywords/Queries/GetHashedKeyword/GetHashedKeywordQuery.cs
+++
@@ -1,7 +0,0 @@
-using Domain.Entities;
-using MediatR;
-
-namespace Application.HashedKeywords.Queries.GetHashedKeyword
-{
-    public record GetHashedKeywordQuery(Keyword Keyword) : IRequest<HashedKeyword>;
-}
